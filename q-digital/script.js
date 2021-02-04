083ad130da04b6cbf1bde960b3195e86460605c9
HRCN <hrcnjk@ya.ru>
2021-02-04 15:23:17 +0400

    Object oriented programming task.

--- q-digital/script.js
+++ q-digital/script.js
@@ -1,73 +1,40 @@
-window.onload = function () {
-    var list = document.getElementById("task-list");
-
-    var todos;
-
-    function toLocal() {
-        todos = list.innerHTML;
-        localStorage.setItem("todos", todos);
+class Plane {
+    name;
+    maxSpeed;
+    state = false;
+    constructor(name, maxSpeed) {
+        this.name = name;
+        this.maxSpeed = maxSpeed;
     }
 
-    list.addEventListener(
-        "click",
-        function (ev) {
-            if (ev.target.tagName === "INPUT") {
-                ev.target.parentNode.classList.toggle("checked");
-                //ev.target.classList.toggle("checked");
-            } else if (ev.target.tagName === "BUTTON") {
-                let div = ev.target.parentNode;
-                div.remove();
-                toLocal();
-            }
-        },
-        false
-    );
-    document
-        .getElementById("addBtn")
-        .addEventListener("click", newElement, false);
-
-    function newElement() {
-        let li = document.createElement("li");
-        let inputValue = document.getElementById("task-input").value;
-        let t = document.createTextNode(inputValue);
-        li.appendChild(t);
-        if (inputValue == "") {
-            alert("Введите ваше дело!");
-        } else {
-            document.getElementById("task-list").appendChild(li);
-        }
-        document.getElementById("task-input").value = "";
-        let checkbox = document.createElement("INPUT");
-        checkbox.type = "checkbox";
-        let deleteBtn = document.createElement("BUTTON");
-        deleteBtn.innerHTML = "Удалить";
-        li.appendChild(checkbox);
-        li.appendChild(deleteBtn);
-        toLocal();
+    takeoff() {
+        this.state = true;
     }
-    document.getElementById("clearAll").addEventListener("click", clearAll);
-
-    function clearAll() {
-        let list = document.getElementById("task-list");
-        list.innerHTML = "";
-        toLocal();
+    landing() {
+        this.state = false;
     }
-    document.getElementById("allDone").addEventListener("click", checkedAll);
-
-    var add = true;
-    function checkedAll() {
-        var li = document.querySelectorAll("li");
-        for (var i = 0; i < li.length; i++) {
-            li[i].classList[add ? "add" : "remove"]("checked");
-            if (add) li[i].firstElementChild.checked = "checked";
-            else li[i].firstElementChild.checked = "";
-        }
-        add = !add;
-
-        toLocal();
+    inAir() {
+        if (this.state) {
+            return (
+                this.name +
+                " В воздухе на скорости " +
+                parseInt(Math.random() * this.maxSpeed) +
+                "км/ч."
+            );
+        } else return this.name + " На земле";
     }
+}
 
-    if (localStorage.getItem("todos")) {
-        list.innerHTML = localStorage.getItem("todos");
+class MilitaryPlane extends Plane {
+    attack() {
+        return this.name + " атакует!";
     }
-};
+}
+
+const plane1 = new MilitaryPlane("МИГ", 1000);
+const plane2 = new Plane("ТУ-154", 700);
+plane1.takeoff();
+
+console.log(plane1.inAir());
+console.log(plane1.attack());
+console.log(plane2.inAir());
